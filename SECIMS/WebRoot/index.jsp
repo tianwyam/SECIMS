24e07a8dacf1cdac134aa90b9ce28f620efff9f8
天涯共明月 <1129064096@qq.com>
2017-03-06 17:14:40 +0800

    first version 1.0（配置环境）

---
+++ SECIMS/WebRoot/index.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"  pageEncoding="UTF-8"%>
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="login">  
+    用户名：<input id="username" name="username" type="text"></input><br>  
+    密  码：<input id="username" name="password" type="password"></input><br>  
+    <input type="submit">  
+</form>  
+</body>
+</html>
---
+++ SECIMS/src/com/cuit/secims/mw/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.cuit.secims.mw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	
+	
+	@RequestMapping("login")
+	public @ResponseBody String hello(String username,String password){
+		
+		
+			return "hello "+username+",your password is: "+password;
+	}
+
+
+}
