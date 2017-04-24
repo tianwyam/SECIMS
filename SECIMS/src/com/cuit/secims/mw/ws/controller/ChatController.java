5ab042388a67ea3b61a73e1b24a10b42bb2ea933
tianyagongmingyue <1129064096@qq.com>
2017-04-24 09:48:44 +0800

    websocket的控制层

---
+++ SECIMS/src/com/cuit/secims/mw/ws/controller/ChatController.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.cuit.secims.mw.ws.controller;
+
+/**
+ * @Description 
+ * @author MW
+ * @date 2017-4-13 下午7:56:30
+ * @version V1.0
+ */
+public class ChatController {
+
+}
