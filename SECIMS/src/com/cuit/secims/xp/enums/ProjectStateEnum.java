e00022aa9ec30eecb16abe3902fd254934949003
xp-zhao <1132813751@qq.com>
2017-05-07 22:24:58 +0800

    项目合作模块（项目状态枚举类）

---
+++ SECIMS/src/com/cuit/secims/xp/enums/ProjectStateEnum.java
@@ -0,0 +1,25 @@
+package com.cuit.secims.xp.enums;
+
+public enum ProjectStateEnum {
+	
+	WAIT(1,"项目等待接收"),
+	PROCESSING(2,"项目进行中"),
+	END(3,"项目结束");
+	
+	private int state;
+	private String stateInfo;
+	
+	private ProjectStateEnum(int state, String stateInfo) {
+		this.state = state;
+		this.stateInfo = stateInfo;
+	}
+	
+	public int getState() {
+		return state;
+	}
+	public String getStateInfo() {
+		return stateInfo;
+	}
+	
+	
+}
