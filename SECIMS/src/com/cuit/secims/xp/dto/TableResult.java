e81d33a7ddc40b78f0ff67e7a123a92ffb20565c
xp-zhao <1132813751@qq.com>
2017-05-07 22:23:58 +0800

    项目合作模块dto层

---
+++ SECIMS/src/com/cuit/secims/xp/dto/TableResult.java
@@ -0,0 +1,47 @@
+package com.cuit.secims.xp.dto;
+
+public class TableResult<T> {
+	
+	private boolean success;
+	
+	private T data;
+	
+	private String error;
+
+	public TableResult(boolean success, T data) {
+		super();
+		this.success = success;
+		this.data = data;
+	}
+
+	public TableResult(boolean success, String error) {
+		super();
+		this.success = success;
+		this.error = error;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+}
