9c919ed7abcfc6fe6dc7fc105550ee4decbd6953
tianyagongmingyue <1129064096@qq.com>
2017-05-09 23:26:21 +0800

    js-学习论坛我的帖子的一些操作

---
+++ SECIMS/WebRoot/res/secims/person/js/learningForumMyPosts.js
@@ -0,0 +1,46 @@
+
+
+
+
+// 删除 我发布的帖子
+function　delPost(postsId){
+	
+	console.log("要删除的帖子ID："+postsId);
+	
+	$.ajax({
+		type:"post",
+		url:"/SECIMS/res/delForumPosts",
+		async:true,
+		dataType:"json",
+		data:{
+			postsId: postsId
+		},
+		success: function(data){
+			
+			if(data.success){
+				
+				layer.msg("删除帖子成功!",{icon:6});
+				
+				// 刷新帖子列表
+				flushForumPostsList(postsId);
+			}
+			
+			console.log("删除帖子成功! "+data);
+		}
+	});
+	
+}
+
+
+// 刷新 帖子列表
+function flushForumPostsList(postsId){
+	
+	// 删除 帖子
+	$("#forumListId"+postsId).remove();
+	
+	// 获取帖子总数
+	var num = $("#postsNum").html();
+	
+	// 修改帖子总数
+	$("#postsNum").html(parseInt(num) - 1);
+}
