bfcecb238ad931ef32d68b9c93fc5853a1fedc64
tianyagongmingyue <1129064096@qq.com>
2017-05-09 23:18:10 +0800

    service - 对学习论坛的操作/聊天消息

--- SECIMS/src/com/cuit/secims/mw/service/LearningForumSV.java
+++ SECIMS/src/com/cuit/secims/mw/service/LearningForumSV.java
@@ -1,6 +1,8 @@
 package com.cuit.secims.mw.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,9 +25,42 @@ public class LearningForumSV {
 	}
 	
 	
+	public List<LearningForumPosts> getForumPostsByconditions(Map map){
+		return this.forumDao.getForumPostsByconditions(map);
+	}
+	
+	/**
+	 * 获取 我的帖子
+	 * @param userid 用户ID
+	 * @return 我的帖子列表  */
+	public List<LearningForumPosts> getForumPostsByUserId(int userid){
+		return this.forumDao.getForumPostsByUserId(userid);
+	}
+	
+	
+	
+	/**
+	 * 获取 我回复的帖子
+	 * @param userid 用户ID
+	 * @return 我回复的帖子列表 */
+	public List<LearningForumPosts> getForumPostsByUserReply(int userid){
+		return this.forumDao.getForumPostsByUserReply(userid);
+	}
+	
+	
 	// 根据ID获取帖子信息
 	public LearningForumPosts getForumPostsById(int postsId) {
-		return this.forumDao.getForumPostsById(postsId);
+		
+		// 每一次查询帖子信息时，都要更新帖子访问量
+		this.forumDao.addBrowseNum(postsId);
+		LearningForumPosts posts = this.forumDao.getForumPostsById(postsId);
+		
+		// 左连接后 子表没有的话，会产生 null
+		if (posts.getChatMsgs() == null || posts.getChatMsgs().get(0).getMsgId() == null) {
+			posts.setChatMsgs(null);
+		}
+		
+		return posts;
 	}
 	
 	
@@ -36,12 +71,28 @@ public class LearningForumSV {
 		
 	
 	
+	// 删除 论坛帖子
+	public int delForumPosts(int postsId){
+		return this.forumDao.delForumPosts(postsId);
+	}
 	
 	
 	
 	
-	
-	
+	// 增加 聊天记录
+	public int addForumChatMsg(String msgContent, int postsId, int userid, int parentId){
+		
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("msgContent", msgContent);
+		map.put("postsId", postsId);
+		map.put("userid", userid);
+		map.put("parentId", parentId);
+
+		// 添加聊天记录时，要增加回复数
+		this.forumDao.addAnswerNum(postsId);
+		
+		return this.forumDao.addForumChatMsg(map);
+	}
 	
 
 }
