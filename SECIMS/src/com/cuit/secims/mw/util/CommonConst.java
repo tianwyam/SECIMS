4505aa4399744fe99f802f44800a034cd8c5212a
tianyagongmingyue <1129064096@qq.com>
2017-05-09 23:20:32 +0800

    util - 对一些常量的总结

---
+++ SECIMS/src/com/cuit/secims/mw/util/CommonConst.java
@@ -0,0 +1,66 @@
+package com.cuit.secims.mw.util;
+
+
+/**
+ * @Description 共用的静态常量值
+ * @author MW
+ * @version V1.0
+ */
+public class CommonConst {
+	
+	
+	/**
+	 * 文件存放路径
+	 */
+	public static final String FILES_DIR = "files" ;
+	
+	/**
+	 *  月评文件目录地址 
+	 */
+	public static final String MONTH_FILE_DIR = "MonthFiles";
+	
+	/**
+	 *  目录分隔符  	
+	 */
+	public static final String DIR_SEPARATOR1 = "\\";
+	
+	
+	/** 
+	 * 目录分隔符 
+	 */
+	public static final String DIR_SEPARATOR2 = "\\\\";
+	
+	
+	/**
+	 * 目录分隔符
+	 */
+	public static final String DIR_SEPARATOR3 = "/";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
