824dde17bf9f3a0fdfffadfb695806a78aa1d8c2
tianyagongmingyue <1129064096@qq.com>
2017-05-10 00:01:19 +0800

    一些测试DB

---
+++ SECIMS/config/DB/select.sql
@@ -0,0 +1,13 @@
+
+
+
+SELECT 
+  * 
+FROM
+  SECIMS.`studyplan` p,
+  SECIMS.`studyplandetail` d 
+WHERE d.`planId` = p.`planId` ;
+
+
+SELECT * FROM SECIMS.`StudyPlan` s LEFT JOIN SECIMS.`StudyPlanDetail` d
+ON s.`planId` = d.`planId` ;
