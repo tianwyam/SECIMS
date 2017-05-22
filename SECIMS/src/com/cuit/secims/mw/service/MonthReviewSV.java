67ab694bab9534d8c810cd94f320fabf6bbf4737
tianyagongmingyue <1129064096@qq.com>
2017-05-22 20:30:16 +0800

    SV - 企业导师评分月评PPT

--- SECIMS/src/com/cuit/secims/mw/service/MonthReviewSV.java
+++ SECIMS/src/com/cuit/secims/mw/service/MonthReviewSV.java
@@ -1,12 +1,15 @@
 package com.cuit.secims.mw.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cuit.secims.mw.dao.IMonthReviewDao;
 import com.cuit.secims.mw.pojo.MonthReview;
+import com.cuit.secims.mw.pojo.MonthReviewResult;
 
 @Service
 public class MonthReviewSV {
@@ -33,5 +36,68 @@ public class MonthReviewSV {
 	public int delMonth(int id){
 		return this.dao.delMonth(id);
 	}
+	
+	
+	
+	
+	//###################### 企业导师 操作 月评PPT ##########################
+	
+	
+	/**
+	 * 月评PPT - 企业导师评语打分
+	 * @param userId 导师用户ID
+	 * @param reviewId 要评论的月评ID
+	 * @param score result结果分数
+	 * @param roleType 角色类型（1：企业导师/2实习生管理员-经理）
+	 * @param comment 评语
+	 * @return
+	 */
+	public int tutorComment(int userId,int reviewId,double score,int roleType,String comment){
+		
+		int count = 0;
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("userId", userId);
+		map.put("reviewId", reviewId);
+		map.put("result", score);
+		map.put("roleType", roleType);
+		map.put("comment", comment);
+		
+		int isExists = this.dao.isExistsMonthComment(map);
+		// 存在，则修改；不存在，则插入
+		if (isExists > 0) {
+			// 修改 打分
+			count = this.dao.tutorMonthModifyComment(map);
+		}else {
+			// 打分
+			count = this.dao.tutorMonthAddComment(map);
+		}
+		
+		return count;
+	}
+	
+	
+	/**
+	 * 获取 月评 评论
+	 * @param userId 用户ID
+	 * @param reviewId 月评ID
+	 * @param roleType 角色类型（1：企业导师/2：企业经理）
+	 * @return 月评评分及评语
+	 */
+	public MonthReviewResult getMonthReviewResult(Integer userId,Integer reviewId,Integer roleType) {
+		
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("userId", userId);
+		map.put("reviewId", reviewId);
+		map.put("roleType", roleType);
+		
+		return this.dao.getMonthReviewResult(map);
+	}
+	
+	
+	
+	
+	
+	
 
 }
