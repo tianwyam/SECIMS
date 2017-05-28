98a1c3e757ac38d15c72bf63064a1387bd563e8d
tianyagongmingyue <1129064096@qq.com>
2017-05-28 10:25:00 +0800

    增加了对菜单的切换等

--- SECIMS/WebRoot/res/jqtreetable/index.js
+++ SECIMS/WebRoot/res/jqtreetable/index.js
@@ -2,8 +2,29 @@ $(function(){
     //菜单点击
     //J_iframe
     $(".J_menuItem").on('click',function(){
+    	
+    	var current = $(this);
+    	
+    	// 修改框架中的SRC
         var url = $(this).attr('href');
         $("#J_iframe").attr('src',url);
+        
+        // 首先清除所有颜色及图标
+        $(".J_menuItem").each(function(index,element){
+        	$(this).css("color","#869fb1");
+        	$(this).removeClass("glyphicon glyphicon-hand-right");
+        });
+        
+        // 当前点击的菜单改变颜色及图标
+        current.css("color","white");
+        current.addClass("glyphicon glyphicon-hand-right");
+        
+        // 返回false，可以提前退出循环
         return false;
     });
+    
+    
+ 
+    
+    
 });
